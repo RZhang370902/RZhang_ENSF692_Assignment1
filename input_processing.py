de9bd7f658bce0910f75d19a4b0fdace3d920b64
“RZhang370902” <zhangruike2413@gmail.com>
2024-05-19 17:51:42 -0600

    assignment 1 step 1

--- input_processing.py
+++ input_processing.py
@@ -18,24 +18,81 @@ class Sensor:
     # Must include a constructor that uses default values
     # You do not need to provide commenting above the constructor
     def __init__(self):
-        pass
+        self.traffic_light_status = "green"
+        self.pedestrian_status = "no"
+        self.vehicle_status = "no"
+        self.last_traffic_light_status = "green"
+        self.program_status = 1
+        
 
     # Replace these comments with your function commenting
-    def update_status(): # You may decide how to implement the arguments for this function
-        pass
+    def update_status(self, user_input): # You may decide how to implement the arguments for this function
+        if user_input == "1":
+            if self.traffic_light_status == "green":
+                self.traffic_light_status = "yellow"
+                self.last_traffic_light_status = "green"
+            elif self.traffic_light_status == "yellow":
+                if self.last_traffic_light_status == "green":
+                    self.traffic_light_status = "red"
+                else:
+                    self.traffic_light_status = "green"
+            else:
+                self.traffic_light_status = "yellow"
+                self.last_traffic_light_status = "red"
+        elif user_input == "2":
+            if self.pedestrian_status == "no":
+                self.pedestrian_status = "yes"
+            else:
+                self.pedestrian_status = "no"
+        elif user_input == "3":
+            if self.vehicle_status == "no":
+                self.vehicle_status = "yes"
+            else:
+                self.vehicle_status = "no"
+        elif user_input == "0":
+            self.program_status = 0
+        else:
+            self.program_status = 2
 
 
 
+sensor1 = Sensor()
+sensor2 = Sensor()
+sensor2.pedestrian_status = "yes"
+
+print(sensor1.vehicle_status)
 # The sensor object should be passed to this function to print the action message and current status
 # Replace these comments with your function commenting
 def print_message(sensor):
-    pass
+    if sensor.program_status == 1:
+        print("\n","Light =",sensor.traffic_light_status,",",
+             "Pedestrian =",sensor.pedestrian_status,",",
+             "Vehicle =",sensor.vehicle_status,"\n")
+    elif sensor.program_status == 0:
+        print("\n Program Ends\n")
+    else:
+        print("Invalid input. Please input 1, 2, 3, or 0")
+        sensor.program_status = 1
 
 
 
 # Complete the main function below
 def main():
+    #Initialize user input
+    user_input = 1
+
     print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
+    
+    while user_input != "0":
+       #print("\n\nCurrent vision status: ")
+        #print("\n","Light =",sensor1.traffic_light_status,",",
+        #    "Pedestrian =",sensor1.pedestrian_status,",",
+        #   "Vehicle =",sensor1.vehicle_status,",","\n")
+        print("\nAre changes detected in the vision input?")
+        user_input = input("Select 1 for light, 2 for pedestrian, 3 for vehicle, or 0 to end the program: ")
+        sensor1.update_status(user_input)
+        #print("\n\nCurrent vision status: ")
+        print_message(sensor1)
 
 
 
